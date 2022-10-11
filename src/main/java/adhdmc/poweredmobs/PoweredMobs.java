5129b3b397ee73f1474bcc75a86138beffbde96f
Peashooter101 <Peashooter101@yahoo.com>
2022-10-10 18:44:47 -0700

    I mean it works...?
    The zombie resurrects after 10s. The original plan of teleport down 1.25 blocks during resurrection is not useful though.
    
    Need to figure out some sort of *animation* for this to work as intended, or at least a placeholder.
    
    TODO: Test stopZombieResurrects()
    TODO: Configurable!
    TODO: On disable, clear out all the zombies on resurrect.

--- src/main/java/adhdmc/poweredmobs/PoweredMobs.java
+++ src/main/java/adhdmc/poweredmobs/PoweredMobs.java
@@ -2,6 +2,7 @@ package adhdmc.poweredmobs;
 
 import adhdmc.poweredmobs.config.Config;
 import adhdmc.poweredmobs.listener.skeleton.SkeletonArrowListener;
+import adhdmc.poweredmobs.listener.zombie.ZombieDeathListener;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -26,6 +27,7 @@ public final class PoweredMobs extends JavaPlugin {
 
     public void registerEvents() {
         getServer().getPluginManager().registerEvents(new SkeletonArrowListener(), this);
+        getServer().getPluginManager().registerEvents(new ZombieDeathListener(), this);
     }
 
     public static void broadcast(String message) {
--- src/main/java/adhdmc/poweredmobs/listener/zombie/ZombieDeathListener.java
+++ src/main/java/adhdmc/poweredmobs/listener/zombie/ZombieDeathListener.java
@@ -3,29 +3,62 @@ package adhdmc.poweredmobs.listener.zombie;
 import adhdmc.poweredmobs.PoweredMobs;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.HashSet;
 
 public class ZombieDeathListener implements Listener {
+
+    public static final NamespacedKey hasResurrected = new NamespacedKey(PoweredMobs.getPlugin(), "has-resurrected");
+    private static final HashSet<Zombie> zombiesOnResurrect = new HashSet<>();
 
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onZombieDeath(EntityDeathEvent event) {
         // Zombie Types: Zombie, Drowned, Husk, PigZombie, VillagerZombie
         if (!(event.getEntity() instanceof Zombie)) return;
         // TODO: Check excluded Zombie Types
+        PersistentDataContainer mobPDC = event.getEntity().getPersistentDataContainer();
+        if (mobPDC.has(hasResurrected)) return;
         event.setCancelled(true);
         Zombie zombie = (Zombie) event.getEntity();
+        zombiesOnResurrect.add(zombie);
         zombie.setAI(false);
         zombie.setInvulnerable(true);
-        zombie.teleport(zombie.getLocation().subtract(0, 1.25, 0));
-        Component zombieName = zombie.name();
+        zombie.setGravity(false);
         Bukkit.getScheduler().runTaskLater(PoweredMobs.getPlugin(), () -> {
+
+            // TODO: Make resurrect chance configurable.
+            if (zombie.isDead()) return;
+            zombie.setAI(true);
+            zombie.setInvulnerable(false);
+            zombie.setGravity(true);
+            AttributeInstance health = zombie.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+            assert health != null;
+            zombie.setHealth(health.getValue());
+            mobPDC.set(hasResurrected, PersistentDataType.BYTE, (byte) 0);
+            zombiesOnResurrect.remove(zombie);
 
         }, 20*10);
         // TODO: ^ Make time configurable.
+    }
+
+    public static void stopZombieResurrects() {
+        for (Zombie zombie : zombiesOnResurrect) {
+            zombie.setInvulnerable(false);
+            AttributeInstance health = zombie.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+            assert health != null;
+            zombie.damage(health.getValue());
+        }
+        zombiesOnResurrect.clear();
     }
 
 }
